f47cad6b952c5fabd3e88b4e6d59941877aa97a6
Dominic Tremblay <dominictremblay24@gmail.com>
2020-12-16 13:01:26 -0500

    final demo

--- server/public/js/app.js
+++ server/public/js/app.js
@@ -1,19 +1,95 @@
 $(document).ready(function () {
-  /* <li class="media">
-  <img class="align-self-start mr-3"
-    src="http://static.tvmaze.com/uploads/images/medium_portrait/246/616301.jpg" alt="Upload">
-  <div class="media-body">
-    <h5 class="mt-0 mb-1">Upload <span class="badge badge-pill badge-info">Comedy</span><span
-        class="badge badge-pill badge-info">Science-Fiction</span><span
-        class="badge badge-pill badge-info">Mystery</span><span
-        class="badge badge-pill badge-secondary"></span></h5>
-    <ul class="list-group">
-      <li class="list-group-item">Premiered: 2020-05-01</li>
-      <li class="list-group-item">Rating: 8.4</li>
-      <li class="list-group-item">Runtime: 30</li>
-    </ul>
-  </div>
-  </li> */
+  const createBadges = (genres) => {
 
-  
+    // ['Comedy','Thriller','Adventures']
+
+    // '' + <span class="badge badge-pill badge-info">Comedy</span>
+    // <span class="badge badge-pill badge-info">Comedy</span> + <span class="badge badge-pill badge-info">Thriller</span>
+    // <span class="badge badge-pill badge-info">Comedy</span> + <span class="badge badge-pill badge-info">Thriller</span> + <span class="badge badge-pill badge-info">Comedy</span> + <span class="badge badge-pill badge-info">Adventures</span>
+
+    let badges = '';
+
+    for (let genre of genres) {
+      badges += `<span class="badge badge-pill badge-info">${genre}</span>`;
+    }
+
+    // <span class="badge badge-pill badge-info">Comedy</span><span class="badge badge-pill badge-info">Thriller</span><span class="badge badge-pill badge-info">Comedy</span><span class="badge badge-pill badge-info">Adventures</span>
+
+    return badges;
+  };
+  // we need to create <li> items
+  const createListItem = (showObj) => {
+    const show = showObj.show;
+
+    const element = `<li class="media">
+    <img class="align-self-start mr-3"
+      src="${show.image.medium}" alt="${show.name}">
+    <div class="media-body">
+      <h5 class="mt-0 mb-1">${show.name} ${createBadges(show.genres)}</h5>
+      <ul class="list-group">
+        <li class="list-group-item">Premiered: ${show.premiered}</li>
+        <li class="list-group-item">Rating: ${show.rating.average || 'n/a'}</li>
+        <li class="list-group-item">Runtime: ${show.runtime} minutes</li>
+      </ul>
+    </div>
+    </li>`;
+
+    return element;
+  };
+
+
+  // add the new li items to the <ul> in the page
+  const renderListItems = showsArr => {
+
+    // loop throught the shows
+    for (let show of showsArr){
+      // create the listItem
+      const listItem = createListItem(show);
+      $('#search-results').append(listItem);
+    }
+
+
+    // add it to the DOM
+
+
+  }
+
+  // Issue a request to the tv api to get the result
+  const getTVShow = (search) => {
+    // create the url for the request
+    const url = `http://api.tvmaze.com/search/shows?q=${search}`;
+
+    // Create an AJAX request GET
+    $.ajax({
+      method: 'GET', // GET,POST,PUT, or DELETE
+      url: url,
+    })
+      .then((result) => {
+        // success. getting the result here
+        // result = respon form the server
+        console.log(result);
+        renderListItems(result);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  // catch the input content
+
+  // catch an event: which one? click on the button? submit on the form?
+  // target the input
+  // get the content of that input
+
+  $('#search-frm').on('submit', function (event) {
+    // stop the form from being submitted
+    event.preventDefault();
+
+    // target the input field -> children
+
+    const $searchBox = $(this).children('input[type="search"]');
+
+    const searchTerm = $searchBox.val();
+
+    getTVShow(searchTerm);
+    $searchBox.val('');
+  });
 });
--- server/public/js/xhr.js
+++ server/public/js/xhr.js
@@ -2,6 +2,29 @@ const ROOT_URL = 'http://localhost:3000/api/superheros/70/biography';
 
 const getBiography = (url) => {
 
+  console.log('issueing request');
+
+  // create the XMLhttp request
+  const xhr = new XMLHttpRequest();
+
+
+  // Initiate the connection 
+  xhr.open('GET', url, true);
+
+  
+  // send request
+  xhr.send();
+
+
+  // listen for the load event
+  xhr.addEventListener('load',function(event) {
+    console.log("displaying result")
+
+    console.log(this.response);
+
+
+  })
+
 };
 
 getBiography(ROOT_URL)
