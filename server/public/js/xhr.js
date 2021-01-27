8c157f85b1cb3b653c098f7a5301715263ba7ede
Dominic Tremblay <dominictremblay24@gmail.com>
2021-01-27 09:28:42 -0500

    demo start

--- server/public/js/xhr.js
+++ server/public/js/xhr.js
@@ -1,30 +1,28 @@
 const ROOT_URL = 'http://localhost:3000/api/superheros/70/biography';
 
 const getBiography = (url) => {
-
   console.log('issueing request');
 
   // create the XMLhttp request
   const xhr = new XMLHttpRequest();
 
-
-  // Initiate the connection 
+  // Initiate the connection
   xhr.open('GET', url, true);
 
-  
   // send request
   xhr.send();
 
-
   // listen for the load event
-  xhr.addEventListener('load',function(event) {
-    console.log("displaying result")
+  xhr.addEventListener('load', function (event) {
+    console.log('displaying result');
 
-    console.log(this.response);
-
-
-  })
-
+    console.log(JSON.parse(this.response));
+  });
 };
 
-getBiography(ROOT_URL)
+const frm = document.getElementById('get-bio-frm');
+
+frm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  getBiography(ROOT_URL);
+});
