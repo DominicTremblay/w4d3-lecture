c25f640caeba290534a51ccf2550fb34d07ab82a
Dominic Tremblay <dominictremblay24@gmail.com>
2021-01-27 09:28:42 -0500

    demo start

---
+++ api_server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+const port = 8000;
+
+app.post('/register', (req, res) => {
+  console.log(req.body);
+
+  res.json({ response: 'success', user: req.body });
+});
+
+app.listen(port, () => console.log(`server is listening on port ${port}`));
