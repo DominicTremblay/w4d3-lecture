688baa8d7509a48fe6ff17b7e9250d57890bb51a
Dominic Tremblay <dominictremblay24@gmail.com>
2021-02-24 12:17:57 -0500

    final demo

--- api_server/server.js
+++ api_server/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 const app = express();
 
+corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
--- server/public/js/cors.js
+++ server/public/js/cors.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
-  
+  $('#register-frm').on('submit', function (event) {
 
+    event.preventDefault();
+    // get the content of the form
+    const content = $(this).serialize();
 
+    $.ajax({
+      url:`http://localhost:8000/register`,
+      method: 'POST',
+      data: content,
+    })
+      .done((result) => {
+        console.log(result);
+      })
+      .fail((err) => console.log(err.message));
+  });
 });
