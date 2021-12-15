7790d7393e7172261fa72ba5330bd30aae8c8112
Dominic Tremblay <dominictremblay24@gmail.com>
2021-12-15 12:23:23 -0500

    final version of the code

--- server/public/js/app.js
+++ server/public/js/app.js
@@ -1,3 +1,66 @@
+const createBadges = function (badgesArr) {
+  let badges = '';
+
+  for (let badge of badgesArr) {
+    badges += `<span class="badge badge-pill badge-info">${badge}</span>`;
+  }
+
+  return badges;
+};
+
+const createShowElement = function (showObj) {
+  const showElement = `
+  <li class="media">
+  <img class="align-self-start mr-3" src="${showObj.show.image.medium}"
+alt="${showObj.show.name}">
+  <div class="media-body">
+  <h5 class="mt-0 mb-1">${showObj.show.name} ${createBadges(showObj.show.genres)}</h5>
+<ul class="list-group">
+<li class="list-group-item">Premiered:${showObj.show.premiered}</li>
+<li class="list-group-item">Rating: ${showObj.show.rating.average}</li>
+<li class="list-group-item">Runtime: ${showObj.show.runtime}</li>
+</ul>
+</div>
+</li>
+`;
+
+  return showElement;
+};
+
+// looping throught the results => [{},{},{}...]
+const renderShowElements = function (showArr) {
+  for (let showObj of showArr) {
+    // Targetting the container and appending the item to it
+    const showElement = createShowElement(showObj);
+    $('#search-results').append(showElement);
+  }
+};
+
+// perform the request and deal with the results
+const getShows = function (searchTerm) {
+  const url = `http://api.tvmaze.com/search/shows?q=${searchTerm}`;
+  // create a request to the tv maze API
+
+  // sending the requet to the TV Maze API
+  $.ajax({
+    url: url,
+    method: 'GET',
+  })
+    .done((results) => {
+      console.log(results); // array of objects
+
+      renderShowElements(results);
+
+      // with the results => create the HTML element => attach to the DOM
+    })
+    .fail((err) => {
+      console.log(`Error: ${err.message}`);
+    })
+    .always(() => {
+      console.log('request to TV Maze done');
+    });
+};
+
 $(document).ready(function () {
   // catch the form submit
 
@@ -11,48 +74,7 @@ $(document).ready(function () {
 
     const searchTerm = inputBox.val();
 
-    const url = `http://api.tvmaze.com/search/shows?q=${searchTerm}`;
-    // create a request to the tv maze API
-
-    // sending the requet to the TV Maze API
-    $.ajax({
-      url: url,
-      method: 'GET',
-    })
-      .done((results) => {
-        console.log(results); // array of objects
-
-        for (let showObj of results) {
-
-          const showElement = `
-              <li class="media">
-        <img class="align-self-start mr-3" src="${showObj.show.image.medium}"
-          alt="${showObj.show.name}">
-        <div class="media-body">
-          <h5 class="mt-0 mb-1">${showObj.show.name} <span class="badge badge-pill badge-info">Comedy</span><span
-              class="badge badge-pill badge-info">Science-Fiction</span><span
-              class="badge badge-pill badge-info">Mystery</span><span class="badge badge-pill badge-secondary"></span></h5>
-          <ul class="list-group">
-            <li class="list-group-item">Premiered:${showObj.show.premiered}</li>
-            <li class="list-group-item">Rating: ${showObj.show.rating.average}</li>
-            <li class="list-group-item">Runtime: ${showObj.show.runtime}</li>
-          </ul>
-        </div>
-      </li>
-        `;
-
-          // Targetting the container and appending the item to it
-
-          $('#search-results').append(showElement);
-        }
-
-        // with the results => create the HTML element => attach to the DOM
-      })
-      .fail((err) => {
-        console.log(`Error: ${err.message}`);
-      })
-      .always(() => {
-        console.log('request to TV Maze done');
-      });
+    // trigger the request
+    getShows(searchTerm);
   });
 });
